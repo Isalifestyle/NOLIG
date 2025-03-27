9f51f0d4e593bc1214069c08d384c65dfd3f24d4
Isalifestyle <oisa@uncc.edu>
2025-03-27 13:25:27 -0400

    small error fix

--- nolig/base/views.py
+++ nolig/base/views.py
@@ -15,7 +15,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import FlashcardSet, FlashCard
+from .models import FlashcardSet, FlashCard, Message
 from .serializers import FlashcardSetSerializer, FlashCardSerializer
 import os
 from django.shortcuts import render
