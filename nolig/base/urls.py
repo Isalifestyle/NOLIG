8207ac46f4fcce02437949908b9930ecf8e75bb7
Isalifestyle <oisa@uncc.edu>
2025-04-04 13:09:27 -0400

    Discussion form comments feature made

--- nolig/base/urls.py
+++ nolig/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+
 from .views import flashcard_feed, createFlashcardSet, updateFlashcardSet, deleteFlashcardSet, updateFlashcard, deleteFlashcard
 from .views import flashcard_feed,flashcard_sets,flashcard_detail 
+
+from .views import flashcard_feed,flashcard_sets,flashcard_detail, user_profile
+
 
 
 urlpatterns = [
@@ -17,12 +21,15 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('discussion/<str:pk>', views.discussion, name='discussion'),
-    
+    path('delete-discussion/<int:pk>/', views.deleteDiscussion, name='delete-discussion'),
+    path('load_more_replies/', views.load_more_replies, name='load-more-replies'),
+
   
     # Routes related to discussions
     path('create-discussion/', views.createDiscussion, name="create-discussion"),
     path('update-discussion/<str:pk>', views.updateDiscussion, name="update-discussion"),
     path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+    path('profile/<int:user_id>/', views.user_profile, name='user_profile'),
 
     # Flashcard feed route
     path('flashfeed/', flashcard_feed, name='flashcard-feed'),
